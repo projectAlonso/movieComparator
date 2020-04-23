434d1a7536e1a31d31f76d8d0b9cbe2d69e58894
Nicolas Alonso <63936366+projectAlonso@users.noreply.github.com>
2020-04-24 00:11:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const autoCompleteConfig = {
         return movie.Title;
     },
     async fetchData (searchTerm) {
-            const response = await axios.get('http://www.omdbapi.com/', {
+            const response = await axios.get('https://www.omdbapi.com/', {
                 params: {
                     apikey: 'e0ef6f6b',
                     s: searchTerm
@@ -41,7 +41,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: 'e0ef6f6b',
             i: movie.imdbID
